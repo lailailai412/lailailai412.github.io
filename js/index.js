887f62ab116cc8d5c0f20ed76f64352f8851c743
lailailai412 <84297701+lailailai412@users.noreply.github.com>
2023-06-13 17:37:57 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-; console.log('�˴����ѻ���,����Դ���롣Դ���ַΨһ����: http://www.bootstrapmb.com/item/9238 '); if (location.href.indexOf('ile:') < 0) { if (location.href.indexOf('stra') < 0) { location.href = 'http://www.bootstrapmb.com/item/9238' } }; var swiper = new Swiper('.blog-slider', {
+; ; var swiper = new Swiper('.blog-slider', {
   spaceBetween: 30,
   effect: 'fade',
   loop: true,
@@ -10,4 +10,4 @@
     el: '.blog-slider__pagination',
     clickable: true,
   }
-});; console.log('�˴����ѻ���,����Դ���롣Դ���ַΨһ����: http://www.bootstrapmb.com/item/9238 '); if (location.href.indexOf('ile:') < 0) { if (location.href.indexOf('stra') < 0) { location.href = 'http://www.bootstrapmb.com/item/9238' } };
+});; 
